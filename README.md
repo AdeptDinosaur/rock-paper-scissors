97871537366a8e0ae230c5f73e608b5c71fcb7f4
AdeptDinosaur <126434367+AdeptDinosaur@users.noreply.github.com>
2023-03-20 20:35:56 -0400

    setup files and provide basic program outline

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # rock-paper-scissors
+Implementation of the rock, paper, scissors game to be played
+in the javascript console.
---
+++ scripts.js
@@ -0,0 +1,3 @@
+//Function to obtain computer's choice
+//Function to play single round of the game with user input
+//Game function to include 5 rounds of a single game
